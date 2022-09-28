f008597b64ad2565fefb2f9db82d80eb8c63c35b
AliMickey1 <alicesonphillips@gmail.com>
2022-09-28 19:25:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -136,9 +136,10 @@ Use the getReviewByIndex function below to do the following:
 */
 
 
-function getReviewByIndex(/*Your code here*/) {
-  /*Your code here*/
+function getReviewByIndex(array, numInd) {
+  return `${array[numInd].name} gave the restaurant a ${array[numInd].rating} star review, and their feedback was: ${array[numInd].feedback}`;
 }
+console.log(getReviewByIndex(reviews,0));
 
   
 
@@ -155,9 +156,12 @@ Use the getLastReview function below to do the following:
 */
 
 
-function getLastReview(/*Your code here*/) {
-  /*Your code here*/
+function getLastReview(array) {
+  return `${array[array.length -1].name} gave the restaurant a ${array[array.length -1].rating} star review, and their feedback was: ${array[array.length - 1].feedback}`;
 } 
+  
+console.log(getLastReview(reviews));
+
 
 
 
